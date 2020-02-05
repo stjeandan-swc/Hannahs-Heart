35413e11cb6ee21a4c81a8292407dfb5d4faf3ef
stjeandan-swc <stjean.dan.dev@gmail.com>
2020-02-05 15:12:31 -0500

    Finished user profile ui

--- Hannahs-Heart/src/app/app.module.ts
+++ Hannahs-Heart/src/app/app.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from '@src/app/app-routing.module';
+
+// Components
 import { AppComponent } from '@src/app/app.component';
 import { HomeComponent } from '@src/app/home/home.component';
-
+import { WelcomeLandingComponent } from '@src/app/welcome-landing/welcome-landing.component';
+import { UserProfileComponent } from '@src/app/user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    WelcomeLandingComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule,
---
+++ Hannahs-Heart/src/app/welcome-landing/welcome-landing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome-landing',
+  templateUrl: './welcome-landing.component.html',
+  styleUrls: ['./welcome-landing.component.css']
+})
+export class WelcomeLandingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
