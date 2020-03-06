6562af6ecc16407b680b23661188a1eb25e98536
stjeandan-swc <stjean.dan.dev@gmail.com>
2020-03-06 11:45:08 -0500

    Fixed scaling issue with alphabeticical search bar. Added fields to the data input component

--- Hannahs-Heart/src/app/enter-data-form/enter-data-form.component.ts
+++ Hannahs-Heart/src/app/enter-data-form/enter-data-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { IDropdownSettings } from 'ng-multiselect-dropdown';
 
 @Component({
   selector: 'app-enter-data-form',
@@ -9,9 +10,32 @@ export class EnterDataFormComponent implements OnInit {
 
   @Input() showForm: boolean;
 
+  genderList = [];
+  selectedItems = [];
+  dropdownSettings = {};
+
   constructor() { }
 
   ngOnInit() {
-  }
+    this.genderList = [
+      { item_id: 1, item_text: 'Male'},
+      { item_id: 2, item_text: 'Female'},
+      { item_id: 3, item_text: 'Trans Male'},
+      { item_id: 4, item_text: 'Trans Female'},
+      { item_id: 5, item_text: 'Other'}
+    ];
 
+    this.selectedItems = [];
+    /*
+    this.dropdownSettings:IDropdownSettings = {
+      singleSelection: false,
+      idField: 'item_id',
+      textField: 'item_text',
+      selectAllText: 'Select All',
+      unSelectAllText: 'UnSelect All',
+      itemsShowLimit: 3,
+      allowSearchFilter: true
+    };
+    */
+  }
 }
